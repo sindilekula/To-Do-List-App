8eb4b78da5d08df6a41815b07b0ff75c593b48b2
sindilekula <Kulasindile@gmail.com>
2022-06-23 16:13:27 +0200

    mobile responsiveness

--- scripts.js
+++ scripts.js
@@ -12,58 +12,11 @@ document.querySelector('#Add').onclick = function () {
    document.querySelector('#list').innerHTML +=`
   <div>
    ${document.querySelector('#ToDo input').value}</div>
+   <button class="delete">Delete</button>
    `
 }
 
 function deleteList() {
     lists.splice(list, 1);
-    localStorage.setItem('records', JSON.stringify(lists));
-}
-
-//   function addReord(e) {
-//     e.preventDefault();
-//     lists.push(
-//         {
-//             id: parseInt(document.getElementById('id').value),
-//             item: document.getElementById('item').value,
-//             createdDate: document.getElementById('date').value,
-//         }
-//     )
-//   };
-
-//   localStorage.setItem()
-
-// let lists = JSON.parse( localStorage.getItem('records')) ?
-// JSON.parse( localStorage.getItem('records')) : [
-//     {
-//         name: 'Lerato',
-//         surname: 'Tebi',
-//         age: 39
-//     }
-// ];
-
-// function addRecord() {
-//     //   e.preventDefault();
-//       lists.push(
-//           {
-//                 name: document.getElementById('name').value,
-//                 surname: document.getElementById('surname').value,
-//                 age: parseInt(document.getElementById('date').value)
-//           }
-//       );
-//       localStorage.setItem('records', JSON.stringify(lists));
-//      };
-
-// function deleteRecord(id) {
-//     lists.splice(id, 1);
 //     localStorage.setItem('records', JSON.stringify(lists));
-// }
-
-// // Load data
-
-// (function loadData() {
-//     console.log(lists)
-// }) ();
-
-// // add event listener
-// document.querySelector('#addRecord').addEventListener('click', addRecord);
+// };
